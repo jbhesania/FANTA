98ab1bb79e278d4be698ec6b750337a8ab68afb1
Nima Azbijari <nazbijar@ucsd.edu>
2018-05-02 21:06:44 -0700

    Added button fields in onCreate

--- app/src/main/java/com/example/android/projectfanta/HomeActivity.java
+++ app/src/main/java/com/example/android/projectfanta/HomeActivity.java
@@ -2,45 +2,22 @@ package com.example.android.projectfanta;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Button homeButton = (Button) findViewById(R.id.Home);
+        Button friendsButton = (Button) findViewById(R.id.Friends);
+        Button addButton = (Button) findViewById(R.id.Add);
+        Button settingsButton = (Button) findViewById(R.id.Settings);
+
     }
 
     // Start Friends Activity
