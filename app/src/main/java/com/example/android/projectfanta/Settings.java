c4d0fccf237c8d9223f483f558b2f4d9bb9485ea
arun <arunsug@gmail.com>
2018-05-06 00:53:29 -0800

    Veiw to click for sign out change

--- app/src/main/java/com/example/android/projectfanta/Settings.java
+++ app/src/main/java/com/example/android/projectfanta/Settings.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -18,15 +20,14 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Settings extends AppCompatActivity {
 
-    private ImageView SignOut;
+    private CardView SignOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        SignOut = (ImageView) findViewById(R.id.log_out);
+        SignOut = (CardView) findViewById(R.id.log_out);
     }
-
 
     public void logOut(View v) {
         switch (v.getId()) {
