cedcb9ff2e03b58ff70b41be48c6a5db4821a740
stef08feb <shartono@ucsd.edu>
2018-05-20 00:25:51 -0700

    fixed nav bar issue, added user profile preview

--- app/src/main/java/com/example/android/projectfanta/ContactsFragment.java
+++ app/src/main/java/com/example/android/projectfanta/ContactsFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.projectfanta;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,7 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -54,6 +56,14 @@ public class ContactsFragment extends Fragment {
         ListViewAdapter lvAdapter = new ListViewAdapter(getContext(), R.layout.itemrow, dataFriends);
 
         list.setAdapter(lvAdapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent contact_intent = new Intent(view.getContext(),UserProfile.class);
+                startActivity(contact_intent);
+            }
+        });
 
         //TODO: get the search function to work
 
