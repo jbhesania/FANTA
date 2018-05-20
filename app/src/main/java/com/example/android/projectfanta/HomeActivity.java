5647e957b5ff049eb9b665d7547cba310f949260
stef08feb <shartono@ucsd.edu>
2018-05-19 21:44:34 -0700

    changed the dimensions of the ui

--- app/src/main/java/com/example/android/projectfanta/HomeActivity.java
+++ app/src/main/java/com/example/android/projectfanta/HomeActivity.java
@@ -1,22 +1,12 @@
 package com.example.android.projectfanta;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-
-import java.lang.reflect.Field;
 
 public class HomeActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/android/projectfanta/LoginActivity.java
+++ app/src/main/java/com/example/android/projectfanta/LoginActivity.java
@@ -186,3 +186,5 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 };
+
+
