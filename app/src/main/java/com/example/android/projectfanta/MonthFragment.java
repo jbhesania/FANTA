72c1156872e2edbb9722210b9b9a408549b4aa2c
Bingtian <bingtianhuang1998@gmail.com>
2018-05-28 14:41:22 -0700

    Graph implemented, still need to feed data to the graph

--- app/src/main/java/com/example/android/projectfanta/MonthFragment.java
+++ app/src/main/java/com/example/android/projectfanta/MonthFragment.java
@@ -1,10 +1,20 @@
 package com.example.android.projectfanta;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LegendRenderer;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.Calendar;
 
 
 public class MonthFragment extends Fragment {
@@ -14,6 +24,62 @@ public class MonthFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_month, container, false);
+
+        // generate Dates
+        Calendar calendar = Calendar.getInstance();
+
+        // Month Field 0-11 Represent January to December
+        calendar.set(2017,11,1);
+
+        Calendar calendar1 = (Calendar)calendar.clone();
+        Calendar calendar2 = (Calendar)calendar.clone();
+
+        DataPoint dp[] = new DataPoint[30];
+        long start = calendar1.getTimeInMillis();
+        for(int i = 0; i < 30; i++)
+        {
+            DataPoint point = new DataPoint(calendar1.getTime(),i+1);
+            dp[i] = point;
+            calendar1.add(Calendar.DATE,1);
+        }
+        long end = calendar1.getTimeInMillis();
+
+        DataPoint dpStd[] = new DataPoint[30];
+        for(int i = 0; i < 30; i++)
+        {
+            dpStd[i] = new DataPoint(calendar2.getTime(),15);
+            calendar2.add(Calendar.DATE,1);
+        }
+
+        GraphView graph = (GraphView) view.findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dp);
+        series.setTitle("User");
+
+        // set date label formatter
+        // use static labels for horizontal and vertical labels
+        // set date label formatter
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(getActivity()));
+        graph.getGridLabelRenderer().setTextSize(36);
+        graph.getGridLabelRenderer().setNumHorizontalLabels(4);
+
+        // set manual x bounds to have nice steps
+        graph.getViewport().setMinX(start);
+        graph.getViewport().setMaxX(end);
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getGridLabelRenderer().setHumanRounding(false);
+
+        graph.getGridLabelRenderer().setHorizontalAxisTitle("date");
+        graph.getGridLabelRenderer().setVerticalAxisTitle("mg");
+
+        LineGraphSeries<DataPoint> standard = new LineGraphSeries<>(dpStd);
+        standard.setTitle("Standard");
+        standard.setColor(Color.GREEN);
+
+        graph.addSeries(series);
+        graph.addSeries(standard);
+        graph.getLegendRenderer().setVisible(true);
+        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.BOTTOM);
+        graph.setTitle("Protein Intake");
 
         return view;
     }
--- app/src/main/java/com/example/android/projectfanta/YearFragment.java
+++ app/src/main/java/com/example/android/projectfanta/YearFragment.java
@@ -1,12 +1,23 @@
 package com.example.android.projectfanta;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LegendRenderer;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.Calendar;
 
 public class YearFragment extends Fragment {
 
@@ -15,6 +26,70 @@ public class YearFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_year, container, false);
+
+        // generate Dates
+        Calendar calendar = Calendar.getInstance();
+
+        // Month Field 0-11 Represent January to December
+        calendar.set(2017,0,1);
+
+        Calendar calendar1 = (Calendar)calendar.clone();
+        Calendar calendar2 = (Calendar)calendar.clone();
+
+        DataPoint dp[] = new DataPoint[12];
+        long start = calendar1.getTimeInMillis();
+        for(int i = 0; i < 12; i++)
+        {
+            DataPoint point = new DataPoint(calendar1.getTime(),i+0.5);
+            dp[i] = point;
+            calendar1.add(Calendar.MONTH,1);
+            Log.e("hLabel",calendar1.get(Calendar.MONTH)+" "+calendar1.get(Calendar.DATE)+"");
+        }
+        long end = calendar1.getTimeInMillis();
+
+        DataPoint dpStd[] = new DataPoint[12];
+        for(int i = 0; i < 12; i++)
+        {
+            dpStd[i] = new DataPoint(calendar2.getTime(),4);
+            calendar2.add(Calendar.MONTH,1);
+        }
+
+        GraphView graph = (GraphView) view.findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dp);
+        series.setTitle("User");
+
+        // set date label formatter
+        // use static labels for horizontal and vertical labels
+        // set date label formatter
+        /*StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
+        staticLabelsFormatter.setHorizontalLabels(new String[] {"January", "February","March",
+                "April","May","June", "July","August","September",
+                "October","November","December"});
+        graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);*/
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(getActivity()));
+        graph.getGridLabelRenderer().setNumHorizontalLabels(7);
+        graph.getGridLabelRenderer().setHorizontalLabelsAngle(15);
+        graph.getGridLabelRenderer().setHumanRounding(false);
+        graph.getGridLabelRenderer().setTextSize(36);
+
+        // set manual x bounds to have nice steps
+        graph.getViewport().setMinX(start);
+        graph.getViewport().setMaxX(end);
+        graph.getViewport().setXAxisBoundsManual(true);
+
+        graph.getGridLabelRenderer().setHorizontalAxisTitle("date");
+        graph.getGridLabelRenderer().setVerticalAxisTitle("mg");
+
+        LineGraphSeries<DataPoint> standard = new LineGraphSeries<>(dpStd);
+        standard.setTitle("Standard");
+        standard.setColor(Color.GREEN);
+
+        graph.addSeries(series);
+        graph.addSeries(standard);
+        graph.getLegendRenderer().setVisible(true);
+        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.BOTTOM);
+        graph.setTitle("Protein Intake");
+
         return view;
     }
 
