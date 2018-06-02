f68f47821a739fba64227541bbf7b6cbe7d75d53
arun <arunsug@gmail.com>
2018-06-02 15:19:50 -0700

    userInfo changes

--- app/src/main/java/com/example/android/projectfanta/UserInfo.java
+++ app/src/main/java/com/example/android/projectfanta/UserInfo.java
@@ -26,17 +26,6 @@ public class UserInfo extends User {
     private double recFat;
 
 
-    private int calories;
-    private int carbs;
-    private int protein;
-    private int sugar;
-    private int sodium;
-    private int fiber;
-    private int potassium;
-    private int cholesterol;
-    private int fat;
-
-
     public UserInfo(){
         super();
     }
@@ -47,6 +36,7 @@ public class UserInfo extends User {
         height = h;
         age = a;
         // we should add the logic for set nutrional values to defulats resetNutrionalValues
+
     }
 
     public UserInfo( UserInfo copy) {
@@ -121,23 +111,5 @@ public class UserInfo extends User {
     public int getWeight() { return weight; }
 
     public int getHeight() { return height; }
-
-    public int getCalories() { return calories; }
-
-    public int getCarbs() { return carbs; }
-
-    public int getFat() { return fat; }
-
-    public int getFiber() { return fiber; }
-
-    public int getSodium() { return sodium; }
-
-    public int getPotassium() { return potassium; }
-
-    public int getCholesterol() { return cholesterol; }
-
-    public int getProtein() { return protein; }
-
-    public int getSugar() { return sugar; }
 
 }
