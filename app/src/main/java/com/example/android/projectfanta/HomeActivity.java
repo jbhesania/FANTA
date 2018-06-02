5fa305fc672a4a7af53b96ef6a26fd4465063784
Joyaan <jbhesania1@gmail.com>
2018-06-02 15:30:59 -0700

    Removed print statement previously changed searching

--- app/src/main/java/com/example/android/projectfanta/HomeActivity.java
+++ app/src/main/java/com/example/android/projectfanta/HomeActivity.java
@@ -13,12 +13,7 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAA");
-        if ( Information.information.getMyFoods() != null && Information.information.getMyFoods() != null) {
-        System.out.println("Foods Size: " + Information.information.getMyFoods().size());
-        System.out.println("Intakes Size: " + Information.information.getMyIntakes().size()); }
-
+        super.onCreate(savedInstanceState); 
         setContentView(R.layout.activity_home);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
