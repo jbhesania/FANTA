ae2084ad1c4bdfcb8827c8b2cbd531e307bfca4f
stef08feb <shartono@ucsd.edu>
2018-06-07 17:09:03 -0700

    setup week, month, year fragment for user

--- app/src/main/java/com/example/android/projectfanta/UserProfile.java
+++ app/src/main/java/com/example/android/projectfanta/UserProfile.java
@@ -4,6 +4,7 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -23,6 +24,8 @@ public class UserProfile extends AppCompatActivity {
     Button select_nutrient;
     RadioGroup rg;
     RadioButton nutrient;
+    private SectionsPageAdapter myAdapters;
+    private ViewPager myPagers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +62,14 @@ public class UserProfile extends AppCompatActivity {
 
             }
         });
+
+        myAdapters = new SectionsPageAdapter(getSupportFragmentManager());
+
+        myPagers = (ViewPager) findViewById(R.id.historyContainer);
+        setUpViewPager(myPagers);
+
+        TabLayout tab = (TabLayout) findViewById(R.id.historyTabs);
+        tab.setupWithViewPager(myPagers);
     }
 
     private void setUpViewPager(ViewPager viewPager){
